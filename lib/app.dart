7d26ef5dd2355720554aa64ff4d7ecc59563127e
Sean <seann287@gmail.com>
2020-04-20 21:58:43 -0700

    Added Sentry.io crashlytics

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,33 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:planet_pet/screens/home.dart';
+import 'package:sentry/sentry.dart';
+
+const sentryDSN = 'https://03af678dd10041d1babef9e30701aeb3@o365156.ingest.sentry.io/5207600';
+final SentryClient sentry = SentryClient(dsn: sentryDSN);
 
 class App extends StatelessWidget {
+  static Future<void> reportError(dynamic error, dynamic stackTrace) async {
+    // if (Foundation.kDebugMode) {
+    //   print(stackTrace);
+    //   return;
+    // }
+
+    final SentryResponse response = await sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace
+    );
+    if (response.isSuccessful) {
+      print('Sentry ID: ${response.eventId}');
+    } else {
+      print('Failed to report to Sentry: ${response.error}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    // throw StateError('Generic Test Error');
+
     return MaterialApp(
       home: Home(),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:planet_pet/app.dart';
+import 'package:sentry/sentry.dart';
+import 'app.dart' as App;
 
-void main() => runApp(App());
+
+
+final SentryClient sentry = SentryClient(dsn: sentryDSN);
+
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    Zone.current.handleUncaughtError(details.exception, details.stack);
+  };
+  runZoned( () {
+    runApp(App.App());
+  }, onError: (error, stackTrace) {
+    App.App.reportError(error, stackTrace);
+  });
+}
