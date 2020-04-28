6474c31e5b02af2737ab13bf742eed87283f43d4
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-04-28 17:08:41 -0400

    Added a basic view for posts.

--- lib/screens/posts.dart
+++ lib/screens/posts.dart
@@ -1,8 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Posts extends StatelessWidget {
+  final CollectionReference postsRef = Firestore.instance.collection('pets');
   @override
   Widget build(BuildContext context) {
-    return Text('Posts');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+          body: StreamBuilder(
+          stream: postsRef.snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (_, index) {
+                  return Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: NetworkImage(
+                          snapshot.data.documents[index]['imageURL']
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 8
+                        ),
+                      ),
+                      Text(
+                        snapshot.data.documents[index]['name']
+                      ),
+                    ],
+                  );
+                }
+              ),
+            );
+          }),
+    );
   }
 }
