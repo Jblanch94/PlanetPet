4718e9270623ef19af98ef4bbe1461a9760a12b1
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-05-02 15:44:24 -0400

    Added displaying of favorited posts.

--- lib/screens/favorite_animals.dart
+++ lib/screens/favorite_animals.dart
@@ -1,6 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:planet_pet/models/user.dart';
 
 class FavoriteAnimals extends StatefulWidget {
   final String userId;
@@ -20,6 +20,9 @@ class _FavoriteAnimalsState extends State<FavoriteAnimals> {
   void initState() {
     super.initState();
     fetchCurrentUserFavorites();
+    if (userFavorites == null) {
+      userFavorites = [];
+    }
   }
 
   void fetchCurrentUserFavorites() async {
@@ -31,28 +34,138 @@ class _FavoriteAnimalsState extends State<FavoriteAnimals> {
     });
   }
 
+  Widget buildPostPet(dynamic record, String url) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: FittedBox(
+                    child: CachedNetworkImage(imageUrl: url),
+                    fit: BoxFit.contain,
+                    alignment: Alignment.topLeft,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text('Name'),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8),
+                              ),
+                              Text('${record['name']}')
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 8)),
+                          Column(
+                            children: <Widget>[
+                              Text('Sex'),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8),
+                              ),
+                              Text('${record['sex']}')
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 8)),
+                          Column(
+                            children: <Widget>[
+                              Text('Animal Type'),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8),
+                              ),
+                              Text('${record['animalType']}')
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 8)),
+                          Column(
+                            children: <Widget>[
+                              Text('Breed'),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8),
+                              ),
+                              Text('${record['breed']}')
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 8)),
+                          Column(
+                            children: <Widget>[
+                              Text('Good with other animals'),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8),
+                              ),
+                              Text(record['goodAnimals'] ? 'Yes' : 'No')
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 8)),
+                          Column(
+                            children: <Widget>[
+                              Text('Leash Required'),
+                              Padding(padding: EdgeInsets.only(top: 8),),
+                              Text(record['leashNeeded'] ? 'Yes' : 'No')
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 8)),
+                          Column(
+                            children: <Widget>[
+                              Text('Availability'),
+                              Padding(padding: EdgeInsets.only(top: 8),),
+                              Text('${record['availability']}')
+                            ],
+                          ),
+                          
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 12)
+                    ),
+                    Text('${record['status']}'),
+                    Padding(padding: EdgeInsets.only(top: 12),),
+                    Text('${record['description']}')
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: postsRef.snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            return ListView.builder(
-                itemCount: snapshot.data.documents.length,
-                itemBuilder: (_, index) {
-                  var record = snapshot.data.documents[index];
-                  String recordId = record.documentID;
+    return Scaffold(
+      body: StreamBuilder(
+          stream: postsRef.snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (_, index) {
+                    DocumentSnapshot record = snapshot.data.documents[index];
+                    String recordId = record.documentID;
 
-                  print(userFavorites.contains(recordId));
 
-                  //TODO: Add pet details for favorited
-                  if (userFavorites.contains(recordId)) {
-                    return Text('valid');
-                  } else return Container();
-                });
-          }
-        });
+                    if (userFavorites.contains(recordId)) {
+                      return buildPostPet(record, record['imageURL']);
+                    } else
+                      return Container();
+                  });
+            }
+          }),
+    );
   }
 }
