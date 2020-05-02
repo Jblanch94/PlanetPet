6d9f239cca398970f6cc8bd9cef77892a7d004f8
Adam Clayman <adam@clayman.org>
2020-05-02 11:57:11 -0400

    Refined admin grid to use CircleAvatar and CachedNetworkImageProvider and display breed in card.

--- lib/screens/admin_grid.dart
+++ lib/screens/admin_grid.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:planet_pet/screens/admin_detail_page.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
   Widget _buildGridItem(BuildContext context, DocumentSnapshot document) {
     return GestureDetector(
       child: Card(
         child: Column(
           children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(document['imageURL']),
-                height: 150,
-                width: 150,
-              )
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: CachedNetworkImageProvider(
+                document['imageURL'],
+              ),
             ),
+            SizedBox(height: 20),
             Text(document['name'], style: TextStyle(fontSize: 14)),
+            Text(document['breed'], style: TextStyle(fontSize: 14)),
             Text(document['availability'], style: TextStyle(fontSize: 14)),
           ],
         ),
