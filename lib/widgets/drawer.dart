f3cca1d25427118b379f0ebb3241f8bee48d7fa0
Sean <seann287@gmail.com>
2020-05-11 19:38:57 -0700

    Fix missing constructor paramters

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,103 +1,25 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsDrawer extends StatefulWidget {
+class SettingsDrawer extends StatelessWidget {
   final bool darkMode;
   final Function(bool) toggleTheme;
   final Function signOut;
   final DocumentSnapshot user;
   final bool detailsPage;
-  const SettingsDrawer(
-      {Key key,
-      this.darkMode,
-      this.toggleTheme,
-      this.user,
-      this.signOut,
-      this.detailsPage})
-      : super(key: key);
-
-  @override
-  _SettingsDrawerState createState() => _SettingsDrawerState();
-}
-
-class _SettingsDrawerState extends State<SettingsDrawer> {
+  const SettingsDrawer({Key key, this.detailsPage, this.darkMode, this.toggleTheme, this.user, this.signOut}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
+          children: <Widget>[
+            SwitchListTile(
+              title: Text('Dark Mode'),
+              value: darkMode,
+              onChanged: toggleTheme,
             ),
-            child: Scrollbar(
-              child: ListView(
-                children: <Widget>[
-                  ListTile(
-                    dense: true,
-                    leading: Icon(Icons.person),
-                    title: Text(widget.user['username']),
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Platform.isAndroid
-                        ? Icon(Icons.phone_android)
-                        : Icon(Icons.phone_iphone),
-                    title: Text(widget.user['phoneNumber']),
-                  ),
-                  Divider(),
-                  ListTile(
-                    dense: true,
-                    leading: Text('Street Address 1:'),
-                    title: Text(widget.user['streetAddress1']),
-                  ),
-                  ListTile(
-                    dense: true,
-                    leading: Text('Street Address 2/Apartment:'),
-                    title: Text(widget.user['streetAddress2']),
-                  ),
-                  ListTile(
-                    dense: true,
-                    leading: Text('City:'),
-                    title: Text(widget.user['city']),
-                  ),
-                  ListTile(
-                    dense: true,
-                    leading: Text('State:'),
-                    title: Text(widget.user['state']),
-                  ),
-                  ListTile(
-                    dense: true,
-                    leading: Text('Zip code:'),
-                    title: Text(widget.user['zipcode']),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SwitchListTile(
-            title: Text('Dark Mode'),
-            value: widget.darkMode,
-            onChanged: widget.toggleTheme,
-          ),
-          RaisedButton(
-            child: Text('Sign Out'),
-            onPressed: () {
-              if (widget.detailsPage) {
-                Navigator.of(context).pop();
-                Navigator.of(context).pop();
-                widget.signOut();
-              } else {
-                Navigator.of(context).pop();
-                widget.signOut();
-              }
-            },
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
   }
 }
