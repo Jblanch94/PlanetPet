7d5c56ebec3bdcdaf1682cf72e41e4c6e8c6bff7
Sean <seann287@gmail.com>
2020-05-11 19:52:22 -0700

    Convert drawer into stateful widget

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,25 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class SettingsDrawer extends StatelessWidget {
+class SettingsDrawer extends StatefulWidget {
   final bool darkMode;
   final Function(bool) toggleTheme;
   final Function signOut;
   final DocumentSnapshot user;
   final bool detailsPage;
-  const SettingsDrawer({Key key, this.detailsPage, this.darkMode, this.toggleTheme, this.user, this.signOut}) : super(key: key);
+  const SettingsDrawer({Key key, this.detailsPage, this.darkMode,
+      this.toggleTheme, this.user, this.signOut}) : super(key: key);
+
+  @override
+  _SettingsDrawerState createState() => _SettingsDrawerState();
+}
+
+class _SettingsDrawerState extends State<SettingsDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-          children: <Widget>[
-            SwitchListTile(
-              title: Text('Dark Mode'),
-              value: darkMode,
-              onChanged: toggleTheme,
-            ),
-          ],
-        ),
-      );
+        children: <Widget>[
+          SwitchListTile(
+            title: Text('Dark Mode'),
+            value: widget.darkMode,
+            onChanged: widget.toggleTheme,
+          ),
+        ],
+      ),
+    );
   }
 }
