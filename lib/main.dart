d5d11b862830e8340778378f5f6cdeee3a25019c
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-05-19 17:30:01 -0400

    Added orientations on main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sentry/sentry.dart';
+import 'package:flutter/services.dart';
 import 'app.dart' as App;
 
 
 const sentryDSN = 'https://03af678dd10041d1babef9e30701aeb3@o365156.ingest.sentry.io/5207600';
 final SentryClient sentry = SentryClient(dsn: sentryDSN);
 
-void main() {
+void main() async {
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp
+  ]);
   FlutterError.onError = (FlutterErrorDetails details) {
     Zone.current.handleUncaughtError(details.exception, details.stack);
   };
