14e99566835d704929b2addb6a5887d3b720ea4f
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-05-23 14:32:36 -0400

    Added reponsiveness to admin detail page

--- lib/screens/admin_detail_page.dart
+++ lib/screens/admin_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
@@ -27,8 +28,12 @@ class AdminDetailPage extends StatefulWidget {
 
 class _AdminDetailPageState extends State<AdminDetailPage> {
   final GlobalKey _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
+    Orientation orientation = MediaQuery.of(context).orientation;
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return CustomScaffold(
         user: widget.userDoc,
         detailsPage: true,
@@ -37,36 +42,47 @@ class _AdminDetailPageState extends State<AdminDetailPage> {
         darkMode: widget.darkMode,
         toggleTheme: widget.toggleTheme,
         title: "${widget.document['name']}'s Details",
-        body: ListView(
-          children: <Widget>[
-            Semantics(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: Image(
-                  image: NetworkImage(widget.document['imageURL']),
-                  height: 300,
-                  width: 300,
-                )
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Semantics(
+                child: Padding(
+                  //padding: EdgeInsets.all(16),
+                  padding: EdgeInsets.all(orientation == Orientation.portrait ? height * 0.035 : width * 0.035),
+                  child: AspectRatio(
+                    //aspectRatio: 4/3,
+                    aspectRatio: orientation == Orientation.portrait ? 4/3 : 9/4,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(orientation == Orientation.portrait ? height * 0.025 : width * 0.03),
+                      child: Image.network(widget.document['imageURL'],
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                label: "${widget.document['name']}'s Image",
+                image: true,
               ),
-              label: "${widget.document['name']}'s Image",
-              image: true,
-            ),
-            Text("Name: ${widget.document['name']}"),
-            Text("Distance: ${double.parse((SphericalUtil.computeDistanceBetween(LatLng(widget.document['latitude'], widget.document['longitude']), LatLng(widget.userDoc['latitude'], widget.userDoc['longitude'])) / 1000 * .621371).toStringAsFixed(1))} miles"),
-            Text("Age: ${Animal(dateOfBirth: widget.document['dateOfBirth'].toDate().toString()).getAge()}"),
-            Text("Birthdate: ${DateFormat.yMMMMEEEEd().format(widget.document['dateOfBirth'].toDate())}"),
-            Text("Sex: ${widget.document['sex']}"),
-            Text("Animal type: ${widget.document['animalType']}"),
-            Text("Breed: ${widget.document['breed']}"),
-            Text("Good with other animals: ${widget.document['goodAnimals']}"),
-            Text("Good with children: ${widget.document['goodChildren']}"),
-            Text("Leash needed: ${widget.document['leashNeeded']}"),
-            Text("Availability: ${widget.document['availability']}"),
-            Text("Status: ${widget.document['status']}"),
-            Text("Description: ${widget.document['description']}"),
-            Text("Latitude: ${widget.document['latitude']}"),
-            Text("Longitude: ${widget.document['longitude']}"),
-          ],
+              Text("Name: ${widget.document['name']}"),
+              Text(
+                  "Distance: ${double.parse((SphericalUtil.computeDistanceBetween(LatLng(widget.document['latitude'], widget.document['longitude']), LatLng(widget.userDoc['latitude'], widget.userDoc['longitude'])) / 1000 * .621371).toStringAsFixed(1))} miles"),
+              Text(
+                  "Age: ${Animal(dateOfBirth: widget.document['dateOfBirth'].toDate().toString()).getAge()}"),
+              Text(
+                  "Birthdate: ${DateFormat.yMMMMEEEEd().format(widget.document['dateOfBirth'].toDate())}"),
+              Text("Sex: ${widget.document['sex']}"),
+              Text("Animal type: ${widget.document['animalType']}"),
+              Text("Breed: ${widget.document['breed']}"),
+              Text(
+                  "Good with other animals: ${widget.document['goodAnimals']}"),
+              Text("Good with children: ${widget.document['goodChildren']}"),
+              Text("Leash needed: ${widget.document['leashNeeded']}"),
+              Text("Availability: ${widget.document['availability']}"),
+              Text("Status: ${widget.document['status']}"),
+              Text("Description: ${widget.document['description']}"),
+              Text("Latitude: ${widget.document['latitude']}"),
+              Text("Longitude: ${widget.document['longitude']}"),
+            ],
+          ),
         ));
   }
 }
